c8f68be0c748caa4d0e2538a4872291a80a8031f
Anthony Corpus <anthonycorpuscarranza@gmail.com>
2026-09-19 19:23:23 -0500

    feat: add staging schema migration

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -5,3 +5,9 @@ CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7002485526
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_<TU_CODIGO>;
+
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7002485526
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7002485526;
